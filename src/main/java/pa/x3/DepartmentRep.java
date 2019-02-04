d66751315af3f3671676dcd2a4c6d34f990071df
Manny <egalli64@gmail.com>
2019-02-04 14:01:04 +0100

    Repo name corrected

--- src/main/java/pa/x3/DepartmentRep.java
+++ src/main/java/pa/x3/DepartmentRep.java
@@ -3,6 +3,6 @@ package pa.x3;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository(value="X3WorkstationRep")
+@Repository(value="X3DepartmentRep")
 public interface DepartmentRep extends CrudRepository<Department, Long> {
 }
