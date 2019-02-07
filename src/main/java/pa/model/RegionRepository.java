624bc315758434e4ac33cd17e0af5baf95bfa7de
Manny <egalli64@gmail.com>
2019-02-07 20:32:37 +0100

    List is actually not useful

--- src/main/java/pa/model/RegionRepository.java
+++ src/main/java/pa/model/RegionRepository.java
@@ -1,12 +1,8 @@
 package pa.model;
 
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RegionRepository extends CrudRepository<Region, Long> {
-	@Override
-	List<Region> findAll();
 }
