3bed02dda75315acf73ca7e2990c436e0531a1b1
Manny <egalli64@gmail.com>
2019-02-20 17:35:44 +0100

    Introducing JPQL on regions

--- src/main/java/pa/controller/RegionCtrl.java
+++ src/main/java/pa/controller/RegionCtrl.java
@@ -15,54 +15,63 @@ import pa.model.RegionRepository;
 
 @Controller
 public class RegionCtrl {
-	private static final Logger logger = LoggerFactory.getLogger(RegionCtrl.class);
+    private static final Logger logger = LoggerFactory.getLogger(RegionCtrl.class);
 
-	@Autowired
-	private RegionRepository repo;
+    @Autowired
+    private RegionRepository repo;
 
-	@GetMapping("/regions")
-	public String showRegions(Model model) {
-		logger.debug("Get all regions");
-		model.addAttribute("regions", repo.findAll());
-		return "regions";
-	}
+    @GetMapping("/regions")
+    public String showRegions(Model model) {
+        logger.debug("Get all regions");
+        model.addAttribute("regions", repo.findAll());
+        return "regions";
+    }
 
-	@GetMapping("/region")
-	public String selectRegion( //
-			@RequestParam long id, //
-			Model model) {
-		logger.debug("Get region " + id);
+    @GetMapping("/region")
+    public String selectRegion( //
+            @RequestParam long id, //
+            Model model) {
+        logger.debug("Get region " + id);
 
-		Optional<Region> opt = repo.findById(id);
-		if(opt.isPresent()) {
-			model.addAttribute("region", opt.get());
-		} else {
-			model.addAttribute("id", id);
-		}
-		return "region";
-	}
+        Optional<Region> opt = repo.findById(id);
+        if (opt.isPresent()) {
+            model.addAttribute("region", opt.get());
+        } else {
+            model.addAttribute("id", id);
+        }
+        return "region";
+    }
 
-	@GetMapping("/region/save")
-	public String saveRegion( //
-			@RequestParam long id, //
-			@RequestParam String name, //
-			Model model) {
-		Region region = new Region(id, name);
-		logger.debug("Save region " + region);
-		repo.save(region);
+    @GetMapping("/region/save")
+    public String saveRegion( //
+            @RequestParam long id, //
+            @RequestParam String name, //
+            Model model) {
+        Region region = new Region(id, name);
+        logger.debug("Save region " + region);
+        repo.save(region);
 
-		model.addAttribute("regions", repo.findAll());
-		return "regions";
-	}
+        model.addAttribute("regions", repo.findAll());
+        return "regions";
+    }
 
-	@GetMapping("/region/delete")
-	public String deleteRegion( //
-			@RequestParam long id, //
-			Model model) {
-		logger.debug("Delete region " + id);
-		repo.deleteById(id);
+    @GetMapping("/region/delete")
+    public String deleteRegion( //
+            @RequestParam long id, //
+            Model model) {
+        logger.debug("Delete region " + id);
+        repo.deleteById(id);
 
-		model.addAttribute("regions", repo.findAll());
-		return "regions";
-	}
+        model.addAttribute("regions", repo.findAll());
+        return "regions";
+    }
+
+    @GetMapping("/my/regions")
+    public String myRegions(Model model) {
+        logger.debug("Get my regions");
+        model.addAttribute("all", repo.myFindAll());
+        model.addAttribute("allNames", repo.myFindAllNames());
+        return "/my/regions";
+    }
+
 }
--- src/main/java/pa/model/RegionRepository.java
+++ src/main/java/pa/model/RegionRepository.java
@@ -1,8 +1,16 @@
 package pa.model;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RegionRepository extends CrudRepository<Region, Long> {
+    @Query("select r from Region r")
+    List<Region> myFindAll();
+
+    @Query("select r.name from Region r")
+    List<String> myFindAllNames();
 }
