f3e42ec66304ead0ca5a56c394eea228637e02a9
Manny <egalli64@gmail.com>
2019-06-11 10:12:58 +0200

    minor cleanup

--- src/main/java/pa/model/Employee.java
+++ src/main/java/pa/model/Employee.java
@@ -14,81 +14,80 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "EMPLOYEES")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "EmpGen")
+    @SequenceGenerator(sequenceName = "EMPLOYEES_SEQ", allocationSize = 1, name = "EmpGen")
+    @Column(name = "EMPLOYEE_ID")
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Date hireDate;
+    private String jobId;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "EmpGen")
-	@SequenceGenerator(sequenceName = "EMPLOYEES_SEQ", allocationSize = 1, name = "EmpGen")
-	@Column(name = "EMPLOYEE_ID")
-	private long id;
-	private String firstName;
-	private String lastName;
-	private String email;
-	private Date hireDate;
-	private String jobId;
+    protected Employee() {
+    }
 
-	protected Employee() {
-	}
+    public Employee(String firstName, String lastName, String jobId) {
+        setPersonalData(firstName, lastName);
+        this.hireDate = Date.valueOf(LocalDate.now());
+        this.jobId = jobId;
+    }
 
-	public Employee(String firstName, String lastName, String jobId) {
-		setPersonalData(firstName, lastName);
-		this.hireDate = Date.valueOf(LocalDate.now());
-		this.jobId = jobId;
-	}
+    public Employee(long id, String firstName, String lastName) {
+        setPersonalData(firstName, lastName);
+        this.id = id;
+    }
 
-	public Employee(long id, String firstName, String lastName) {
-		setPersonalData(firstName, lastName);
-		this.id = id;
-	}
-	
-	private void setPersonalData(String firstName, String lastName) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		setDefaultEmail();
-	}
+    private void setPersonalData(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        setDefaultEmail();
+    }
 
-	public long getId() {
-		return id;
-	}
+    public long getId() {
+        return id;
+    }
 
-	public String getFirstName() {
-		return firstName;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-	public Date getHireDate() {
-		return hireDate;
-	}
+    public Date getHireDate() {
+        return hireDate;
+    }
 
-	public String getJobId() {
-		return jobId;
-	}
+    public String getJobId() {
+        return jobId;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;		
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public void setDefaultEmail() {
-		this.email = firstName.charAt(0) + lastName.substring(0, Math.min(7, lastName.length())).toUpperCase();		
-	}
+    public void setDefaultEmail() {
+        this.email = firstName.charAt(0) + lastName.substring(0, Math.min(7, lastName.length())).toUpperCase();
+    }
 
-	@Override
-	public String toString() {
-		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
-				+ ", hireDate=" + hireDate + ", jobId=" + jobId + "]";
-	}
+    @Override
+    public String toString() {
+        return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+                + ", hireDate=" + hireDate + ", jobId=" + jobId + "]";
+    }
 }
